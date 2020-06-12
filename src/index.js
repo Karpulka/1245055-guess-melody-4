e4d7e3d5d9e4b6cb644931a771257a9879e1fad0
Alyona Karpova <karpova.dev@yandex.ru>
2020-06-12 12:54:46 +0300

    реализован компонент WelcomScreen и передача в него пропсов

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
 
+const ERRORS_COUNT = 5;
+
 const init = () => {
+  const settings = {
+    errorsCount: ERRORS_COUNT
+  };
+
   ReactDOM.render(
-    <App />,
-    document.querySelector(`#root`)
+      <App settings={settings}/>,
+      document.querySelector(`#root`)
   );
 };
 
