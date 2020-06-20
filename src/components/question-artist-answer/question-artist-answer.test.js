7fbcea7d518e6e2832a0b0fea51cffb6cb4752e3
Alyona Karpova <karpova.dev@yandex.ru>
2020-06-20 20:10:05 +0300

    добавлен снепшот-тест для компонента QuestionArtistAnswer

---
+++ src/components/question-artist-answer/question-artist-answer.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import QuestionArtistAnswer from "./question-artist-answer.jsx";
+
+const question = {
+  type: `artist`,
+  song: {
+    answer: ``,
+    src: ``
+  },
+  answers: [
+    {
+      answer: `one`,
+      src: `pic-one`,
+    },
+    {
+      answer: `two`,
+      src: `pic-two`,
+    },
+    {
+      answer: `three`,
+      src: `pic-three`,
+    },
+  ]
+};
+
+const answer = {
+  src: `pic-one`,
+  answer: `one`
+};
+
+const handleChangeAnswer = () => {};
+
+it(`Render QuestionArtistAnswer component`, () => {
+  const tree = renderer
+    .create(<QuestionArtistAnswer
+      answer={answer}
+      onChangeAnswer={handleChangeAnswer}
+      question={question}/>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
